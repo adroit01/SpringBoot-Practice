202678ecbd1f25eeb87dd0edd9d3b37af4873c9d
adroit01 <Amit.keshri01@gmail.com>
2017-12-23 15:39:53 +0530

    BackUp2 UserService With ExceptionHandling

---
+++ rest-demo/src/main/java/com/adroit/rest/webservices/restdemo/exception/UserAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.adroit.rest.webservices.restdemo.exception;
+
+public class UserAlreadyExistException extends Exception {
+
+	public UserAlreadyExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
